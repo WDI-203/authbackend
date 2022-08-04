65d95cc368021794ddcf3d10c0613c00a552a451
jnissenbaum18 <jnissenbaum@verizon.net>
2022-08-04 11:14:18 -0400

    Work In Progress: User Page Permissioning

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const bcrypt = require("bcryptjs");
 const { uuid } = require("uuidv4");
 const { blogsDB } = require("../mongo");
+const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+dotenv.config();
 
 const createUser = async (username, passwordHash) => {
   try {
@@ -46,9 +49,30 @@ router.post("/login-user", async (req, res) => {
     const user = await collection.findOne({
       username,
     });
+    console.log(user)
+    if (!user) {
+      res.json({ success: false }).status(204);
+      return;
+    }
+
     const match = await bcrypt.compare(password, user.password);
+    const jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+    const userType = username.includes("codeimmersives.com") ? "admin" : "user"
+
+    const data = {
+      time: new Date(),
+      userId: user.uid, // Note: Double check this line of code to be sure that user.uid is coming from your fetched mongo user
+      userType: userType,
+      
+    };
+    console.log(match)
+    const expiration = Math.floor(Date.now() / 1000) + (60 * 60)
+    const token = jwt.sign({data, exp: expiration}, jwtSecretKey);
+    console.log(token)
+    res.json({ success: true, token, userType });
+    return;
     if (match) {
-      res.json({ success: true });
       return;
     }
     res.json({ success: false });
@@ -61,5 +85,32 @@ router.post("/login-user", async (req, res) => {
 router.get("/hello-auth", (req, res) => {
   res.json({ message: "Hello from auth" });
 });
+
+router.get("/validate-token", (req, res) => {
+  const tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  const jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  try {
+    const token = req.header(tokenHeaderKey);
+    const verified = jwt.verify(token, jwtSecretKey);
+
+    console.log("token ", token)
+    console.log("verified ", verified)
+
+    if (verified && verified.scope === "admin") {
+      return res.json({ success: true, message: "Speak 'Friend' and enter..." });
+    }
+    if (verified && verified.scope === "user") {
+      return res.json({ success: true, message: "You shall not pass!" });
+    }
+
+    throw Error("Access Denied");
+  } catch (error) {
+    // Access Denied
+    return res.status(401).json({ success: true, message: String(error) });
+  }
+});
+
+
 
 module.exports = router;
