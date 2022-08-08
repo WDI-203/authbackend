01fba236bf7ee92e425a7e91e177af902a9a0600
jnissenbaum18 <jnissenbaum@verizon.net>
2022-08-07 20:26:04 -0400

    Implemented admin authorization functionality

--- routes/auth.js
+++ routes/auth.js
@@ -49,36 +49,38 @@ router.post("/login-user", async (req, res) => {
     const user = await collection.findOne({
       username,
     });
-    console.log(user)
+
     if (!user) {
-      res.json({ success: false }).status(204);
+      res.json({ success: false, message: "Could not find user." }).status(204);
       return;
     }
 
     const match = await bcrypt.compare(password, user.password);
-    const jwtSecretKey = process.env.JWT_SECRET_KEY;
+    if (!match) {
+      res
+        .json({ success: false, message: "Password was incorrect." })
+        .status(204);
+      return;
+    }
 
-    const userType = username.includes("codeimmersives.com") ? "admin" : "user"
+    const jwtSecretKey = process.env.JWT_SECRET_KEY;
+    const expiration = Math.floor(Date.now() / 1000) + 60 * 60;
+
+    const userType = username.includes("codeimmersives.com") ? "admin" : "user";
 
     const data = {
       time: new Date(),
       userId: user.uid, // Note: Double check this line of code to be sure that user.uid is coming from your fetched mongo user
-      userType: userType,
-      
+      scope: userType,
     };
-    console.log(match)
-    const expiration = Math.floor(Date.now() / 1000) + (60 * 60)
-    const token = jwt.sign({data, exp: expiration}, jwtSecretKey);
-    console.log(token)
+
+    const token = jwt.sign({ data, exp: expiration }, jwtSecretKey);
+
     res.json({ success: true, token, userType });
     return;
-    if (match) {
-      return;
-    }
-    res.json({ success: false });
-  } catch (e) {
-    console.error(e);
-    res.json({ success: false });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: e });
   }
 });
 
@@ -86,31 +88,35 @@ router.get("/hello-auth", (req, res) => {
   res.json({ message: "Hello from auth" });
 });
 
-router.get("/validate-token", (req, res) => {
-  const tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
-  const jwtSecretKey = process.env.JWT_SECRET_KEY;
-
+router.get("/validate-admin", (req, res) => {
   try {
+    const tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    const jwtSecretKey = process.env.JWT_SECRET_KEY;
     const token = req.header(tokenHeaderKey);
     const verified = jwt.verify(token, jwtSecretKey);
-
-    console.log("token ", token)
-    console.log("verified ", verified)
-
-    if (verified && verified.scope === "admin") {
-      return res.json({ success: true, message: "Speak 'Friend' and enter..." });
+    
+    if (!verified) {
+        return res.json({ success: false, isAdmin: false });
     }
-    if (verified && verified.scope === "user") {
-      return res.json({ success: true, message: "You shall not pass!" });
+    
+    const userData = verified.data
+
+    if (userData && userData.scope === "admin") {
+      return res.json({
+        success: true,
+        isAdmin: true,
+      });
+    }
+
+    if (userData && userData.scope === "user") {
+      return res.json({ success: true, isAdmin: false });
     }
 
     throw Error("Access Denied");
   } catch (error) {
     // Access Denied
-    return res.status(401).json({ success: true, message: String(error) });
+    return res.status(401).json({ success: false, message: error });
   }
 });
-
-
 
 module.exports = router;
